39905a772e2ba3d1f4bd7959fb28f1db793cf2a8
G2N1337 <groovestreet654@gmail.com>
2022-04-16 22:10:35 +0700

    быстрофикс

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,13 +71,6 @@ const Home: NextPage = () => {
 		}
 	);
 
-	const toBase64 = (file: Blob) =>
-		new Promise((resolve, reject) => {
-			const reader = new FileReader();
-			reader.readAsDataURL(file);
-			reader.onload = () => resolve(reader.result);
-			reader.onerror = (error) => reject(error);
-		});
 	return (
 		<Page>
 			<PhotoBackground>
